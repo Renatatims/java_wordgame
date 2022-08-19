0c0ef20732f8bfbd31a109068f1687076e4f645e
ReTims <renatatims@gmail.com>
2022-08-18 23:21:01 -0500

    Added an event Listener to the Start Button

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-var startButton = document.getElementById('start');
+var startButton = document.getElementById("start");
+startButton.setAttribute("style", "color:blue; background-color:aqua; font-size:30px; font-weight:bolder; display:flex; justify-content:center; text-align:center;"); 
 
-var guess = document.getElementById ("guess");
+var guess = document.getElementById("guess");
 
 var timeX = document.querySelector(".time");
 
-var win = document.getElementById ("result");
+timeX.setAttribute("style", "color:blue; background-color:aqua; font-size:30px; font-weight:bolder; display:flex; flex-wrap:wrap; justify-content:center; ");
 
-var loose = document.getElementById ("result");
+var secondsLeft = 30;
+var win = document.getElementById("result");
+
+var loose = document.getElementById("result");
 
 
 
+startButton.addEventListener ("click", function () {
+	var timer = setInterval(function() {
+		secondsLeft--;
+		timeX.textContent = secondsLeft + " seconds left";
+		if (secondsLeft === 0) {
+			clearInterval(timer);
+			timeX.textContent = "Time ended!";
+			sendMessage();
+}
+}, 1000);
 
+});
+
+
+function sendMessage() {
+	win.textContent = "You won!"
+	result.appendChild(win);
+}

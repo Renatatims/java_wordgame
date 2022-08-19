4235ccc8eaf59833e201822271285ac181b2ec02
ReTims <renatatims@gmail.com>
2022-08-18 23:07:17 -0500

    index, script and CSS added

---
+++ script.js
@@ -0,0 +1,13 @@
+var startButton = document.getElementById('start');
+
+var guess = document.getElementById ("guess");
+
+var timeX = document.querySelector(".time");
+
+var win = document.getElementById ("result");
+
+var loose = document.getElementById ("result");
+
+
+
+
